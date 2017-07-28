519ce264953d482170e2e66c49d305ee0b6a7404
GanZiB4fun <ganzib4fun@163.com>
2017-07-28 16:46:30 +0800

    xadmin 后台系统全局设置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -5,8 +5,20 @@
 # @File    : adminx.py
 
 import xadmin
-from .models import Banner
-from .models import EmailVerifyRecord
+from xadmin import views
+
+from .models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+	enable_themes = True
+	use_bootswatch = True
+
+
+class GlobalSetting(object):
+	site_title = "多功能智障儿后台管理系统"
+	site_footer = "多功能智障儿在线"
+	menu_style = "accordion"
 
 
 class EmailVerifyRecordAdmin(object):
@@ -23,3 +35,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
